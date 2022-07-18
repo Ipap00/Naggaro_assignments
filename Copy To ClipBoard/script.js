6e87d08a2152618fa11e6aab5829a8a4fdfe084a
Ipap00 <98342323+Ipap00@users.noreply.github.com>
2022-07-18 12:25:25 +0530

    copy sc updated

--- Copy To ClipBoard/script.js
+++ Copy To ClipBoard/script.js
@@ -1,18 +1,26 @@
-$(document).ready(function () {
-    $("#add-btn").click(function () {
-        if ($("#task-input").val() != "") {
-            $("#todo").append(
-                $('<div class="row justify-content-center align-items-center text-white my-2">' +
-                    '<p class="col-sm-8 m-0 bg-dark py-2 rounded">' +
-                    $("#task-input").val() +
-                    '</p>' +
-                    '<i class="col-sm-1 text-center fas fa-trash text-danger py-2"></i>' +
-                    '</div>')
-            );
-            $("#task-input").val("");
-            $("i.fas.fa-trash").click(function () {
-                $(this).parent().remove();
-            });
-        }
-    });
+$("#copy-text").val('A total of 18 students from West Bengal were among the 110 who figure in the top three rank holders list of the Indian Certificate of Secondary Education (ICSE) exams, the results of which were announced on Sunday. Nine class 10 students of Bengal got the all India second rank - six of them boys and three girls - with 498 marks out of 500.')
+
+$("#copy-btn").click( () => {
+    var copyText = document.getElementById("copy-text");
+    copyText.select();
+    copyText.setSelectionRange(0, 1000);
+    navigator.clipboard.writeText(copyText.value);
+    $("#copy-btn").html("Copied!");
+    setTimeout(() => {
+        $("#copy-btn").html("Copy");
+    }, 1000);
+});
+
+$("#paste-btn").click( async () => {
+  try {
+    var text = await navigator.clipboard.readText();
+    $("#paste-text").val(text);
+    $("#paste-btn").html("Pasted!");
+    setTimeout(() => {
+        $("#paste-btn").html("Paste");
+    }, 1000);
+  }
+  catch (error) {
+    alert(error);
+  }
 });
